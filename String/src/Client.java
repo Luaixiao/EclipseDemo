07454b7de23fbb295a1d1add3d00f7d777b7caf3
zhaolulu <zhaolulu@DESKTOP-9D4C0RS>
2018-11-17 22:24:03 +0800

    TCP客户端和服务端通信

---
+++ String/src/Client.java
@@ -0,0 +1,24 @@
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class Client {
+	public static void main(String args[])
+	{
+		try {
+			Socket s = new Socket("127.0.0.1",8888);
+			InputStream is =s.getInputStream();
+			DataInputStream dis = new DataInputStream(is);
+			System.out.println(dis.readUTF());
+			dis.close();
+			s.close();
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+			System.out.println("服务器连接失败！！");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ String/src/Server.java
@@ -0,0 +1,29 @@
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+//服务端
+public class Server {
+	public static void main(String args[])
+	{
+		try {
+			ServerSocket s = new ServerSocket(8888);     //创建ServerSocket对象，并指定端口号为8888
+			while(true)       //循环接收消息
+			{
+				Socket ss = s.accept();   //监听并接收套接字
+				OutputStream os = ss.getOutputStream();
+				DataOutputStream dos = new DataOutputStream(os);
+				dos.writeUTF("客户端IP："+ss.getInetAddress().getHostAddress()+"客户端端口号:"+ss.getPort());
+				dos.close();
+				ss.close();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.out.println("程序出错！！");
+		} catch (Exception e){
+			e.printStackTrace();
+		}
+		
+	}
+}
